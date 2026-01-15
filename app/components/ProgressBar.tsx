015afdf3c4b45bc32dd60b30e92a3af219d88e76
Кутузов Андрей <a.kutuzov@2035.university>
2026-01-15 22:35:59 +0300

    feat: add component TaskRow

--- app/components/ProgressBar.tsx
+++ app/components/ProgressBar.tsx
@@ -4,7 +4,7 @@ type ProgressBarProps = {
 
 export const ProgressBar = ({ percent = 0 }: ProgressBarProps) => {
   return (
-    <div className="border-solid border-[1px] border-[#EFEFEF] p-[2px] rounded-sm">
+    <div className="border-solid border-[1px] border-gray-500 p-[2px] rounded-sm">
       <div
         style={{ width: `${percent}%` }}
         className="bg-[#EFEFEF] h-1 rounded-sm"
--- app/quests/page.tsx
+++ app/quests/page.tsx
@@ -1,9 +1,46 @@
 import { HeaderQuests } from "@/app/components/HeaderQuests";
+import { TaskRow } from "@/app/components/TaskRow";
+import { TaskRowType } from "@/app/types/TaskRow";
 
 export default function Page() {
+  const tasks: TaskRowType[] = [
+    {
+      id: 1,
+      title: "Сделать дизайн",
+      description: "Зафиналить MVP",
+      checked: false,
+    },
+    {
+      id: 2,
+      title: "Сделать дизайн",
+      description: "Зафиналить MVP",
+      checked: true,
+    },
+    {
+      id: 3,
+      title: "Сделать дизайн",
+      description: "Зафиналить MVP",
+      checked: false,
+    },
+    {
+      id: 4,
+      title: "Сделать дизайн",
+      description: "Зафиналить MVP",
+      checked: false,
+    },
+  ];
+
   return (
     <div className="w-full max-w-sm mx-auto min-h-screen p-4">
       <HeaderQuests />
+      <div className="mt-5">
+        <h2 className="font-bold text-2xl text-gray-500">Мои Квесты</h2>
+        <div className="mt-4 flex flex-col gap-3">
+          {tasks.map((task) => (
+            <TaskRow key={task.id} data={task} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
