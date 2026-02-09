97c072233fa65a867d4af13ebb771548e5347938
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-09 17:50:34 +0300

    feat(frontend): add hook for check get data from localStorage and add component Loader

--- src/store/questsStore.ts
+++ src/store/questsStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import { TaskRowType } from "@/src/types/TaskRow";
 
 interface QuestsState {
@@ -10,54 +11,66 @@ interface QuestsState {
 }
 
 const initialTasks: TaskRowType[] = [
-  {
-    id: 1,
-    title: "Сделать дизайн",
-    description: "Зафиналить MVP",
-    checked: false,
-  },
-  {
-    id: 2,
-    title: "Сделать дизайн",
-    description: "Зафиналить MVP",
-    checked: false,
-  },
-  {
-    id: 3,
-    title: "Сделать дизайн",
-    description: "Зафиналить MVP",
-    checked: false,
-  },
-  {
-    id: 4,
-    title: "Сделать дизайн",
-    description: "Зафиналить MVP",
-    checked: false,
-  },
+  // {
+  //   id: 1,
+  //   title: "Сделать дизайн",
+  //   description: "Зафиналить MVP",
+  //   checked: false,
+  // },
+  // {
+  //   id: 2,
+  //   title: "Сделать дизайн",
+  //   description: "Зафиналить MVP",
+  //   checked: false,
+  // },
+  // {
+  //   id: 3,
+  //   title: "Сделать дизайн",
+  //   description: "Зафиналить MVP",
+  //   checked: false,
+  // },
+  // {
+  //   id: 4,
+  //   title: "Сделать дизайн",
+  //   description: "Зафиналить MVP",
+  //   checked: false,
+  // },
 ];
 
-export const useQuestsStore = create<QuestsState>((set) => ({
-  tasks: initialTasks,
+export const useQuestsStore = create<QuestsState>()(
+  persist(
+    (set) => ({
+      tasks: initialTasks,
 
-  addTask: (task) =>
-    set((state) => {
-      const maxId =
-        state.tasks.length > 0 ? Math.max(...state.tasks.map((t) => t.id)) : 0;
-      return { tasks: [...state.tasks, { id: maxId + 1, ...task }] };
+      addTask: (task) =>
+        set((state) => {
+          const maxId =
+            state.tasks.length > 0
+              ? Math.max(...state.tasks.map((t) => t.id))
+              : 0;
+          return { tasks: [...state.tasks, { id: maxId + 1, ...task }] };
+        }),
+
+      removeTask: (id) =>
+        set((state) => ({ tasks: state.tasks.filter((t) => t.id !== id) })),
+
+      toggleTask: (id) =>
+        set((state) => ({
+          tasks: state.tasks.map((t) =>
+            t.id === id ? { ...t, checked: !t.checked } : t
+          ),
+        })),
+
+      updateTask: (id, updates) =>
+        set((state) => ({
+          tasks: state.tasks.map((t) =>
+            t.id === id ? { ...t, ...updates } : t
+          ),
+        })),
     }),
-
-  removeTask: (id) =>
-    set((state) => ({ tasks: state.tasks.filter((t) => t.id !== id) })),
-
-  toggleTask: (id) =>
-    set((state) => ({
-      tasks: state.tasks.map((t) =>
-        t.id === id ? { ...t, checked: !t.checked } : t
-      ),
-    })),
-
-  updateTask: (id, updates) =>
-    set((state) => ({
-      tasks: state.tasks.map((t) => (t.id === id ? { ...t, ...updates } : t)),
-    })),
-}));
+    {
+      name: "quests-storage", // Ключ в localStorage
+      storage: createJSONStorage(() => localStorage), // Используем localStorage
+    }
+  )
+);
