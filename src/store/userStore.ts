caf81e75224c9148b9f71fa3d88e6f9651b66334
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-09 18:04:54 +0300

    feat(frontend): save user store in localStorage

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,11 +1,20 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface UserState {
   exp: number;
   addExp: (countExp: number) => void;
 }
 
-export const useUserStore = create<UserState>((set) => ({
-  exp: 800,
-  addExp: (countExp) => set((state) => ({ exp: state.exp + countExp })),
-}));
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      exp: 0,
+      addExp: (countExp) => set((state) => ({ exp: state.exp + countExp })),
+    }),
+    {
+      name: "user-storage", // Ключ в localStorage
+      storage: createJSONStorage(() => localStorage), // Используем localStorage
+    }
+  )
+);
