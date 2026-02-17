82be9c60f60f553fbf2e875f99795484de11ceb3
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-17 22:47:11 +0300

    feat: added props size for ProgressBar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,14 +1,26 @@
+import { clsx } from "clsx";
+
 type ProgressBarProps = {
   percent: number;
+  size?: "small" | "large";
 };
 
-export const ProgressBar = ({ percent = 0 }: ProgressBarProps) => {
+export const ProgressBar = ({
+  percent = 0,
+  size = "small",
+}: ProgressBarProps) => {
+  const classNames = clsx(
+    "bg-[#EFEFEF]",
+    size === "small" ? "h-1 rounded-sm" : "h-[18px] rounded-xs"
+  );
+  const classNamesBorder = clsx(
+    "border-solid border-[1px] border-gray-500 p-[2px]",
+    size === "small" ? "rounded-sm" : "rounded-xs"
+  );
+  console.log("size", size);
   return (
-    <div className="border-solid border-[1px] border-gray-500 p-[2px] rounded-sm">
-      <div
-        style={{ width: `${percent}%` }}
-        className="bg-[#EFEFEF] h-1 rounded-sm"
-      ></div>
+    <div className={classNamesBorder}>
+      <div style={{ width: `${percent}%` }} className={classNames}></div>
     </div>
   );
 };
