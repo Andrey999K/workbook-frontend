84b9bbb972bedc69bc7208c60228f39ff2fa2200
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-18 19:58:10 +0300

    feat: added real values for level and current level progress and experience in profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import { Wrapper } from "@/src/components/Wrapper";
 import Image from "next/image";
 import { ProgressBar } from "@/src/components/ProgressBar";
 import { CircleButton } from "@/src/components/common/CircleButton";
+import { useUserStore } from "@/src/store";
+import { getLevel } from "@/src/utils/getLevel";
 
 export default function ProfilePage() {
+  const userExp = useUserStore((state) => state.exp);
+  const { level, progressLevel, expForLevel, currentHpLevel } =
+    getLevel(userExp);
+
   return (
     <Wrapper>
       <div className="flex flex-col items-center justify-between py-5 min-h-screen -my-4">
@@ -17,12 +25,14 @@ export default function ProfilePage() {
             />
           </div>
           <h3 className="font-bold text-5xl">Андрей</h3>
-          <span className="font-bold text-base mt-3">1 уровень</span>
+          <span className="font-bold text-base mt-3">{level} уровень</span>
         </div>
         <div className="w-full flex flex-col">
-          <span className="font-bold">740 опыта</span>
-          <ProgressBar size="large" percent={60} />
-          <span className="ml-auto text-white/30 font-bold">1000 опыта</span>
+          <span className="font-bold">{currentHpLevel} опыта</span>
+          <ProgressBar size="large" percent={progressLevel} />
+          <span className="ml-auto text-white/30 font-bold">
+            {expForLevel} опыта
+          </span>
         </div>
         <div className="flex items-center gap-4">
           <CircleButton icon="list" />
