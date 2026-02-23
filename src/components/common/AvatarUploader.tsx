7d46418c6bfe38135477da3fef325ca4942b94e9
Кутузов Андрей <a.kutuzov@2035.university>
2026-02-23 22:09:36 +0300

    feat: add avatar persistence in localtorage

--- src/components/common/AvatarUploader.tsx
+++ src/components/common/AvatarUploader.tsx
@@ -3,19 +3,26 @@ import { Avatar } from "@/src/components/common/Avatar";
 import { Icon } from "@/src/components/common/Icon";
 
 export const AvatarUploader = () => {
-  const [preview, setPreview] = useState<string | null>(null);
+  const [preview, setPreview] = useState<string | null>(() =>
+    localStorage.getItem("avatar")
+  );
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
 
-    console.log("name:", file.name);
-    console.log("size:", file.size);
-    console.log("type:", file.type);
-    console.log("lastModified:", file.lastModified);
+    // if (file.size > 100 * 1024) {
+    //   console.log("Файл больше 100KB:", file.size, "байт");
+    //   return;
+    // }
 
-    const url = URL.createObjectURL(file);
-    setPreview(url);
+    const reader = new FileReader();
+    reader.onload = () => {
+      const base64 = reader.result as string; // "data:image/png;base64,..."
+      setPreview(base64);
+      localStorage.setItem("avatar", base64);
+    };
+    reader.readAsDataURL(file);
   };
 
   return (
